4dc63bc5632e57583cdf9bf3ffd736acf38f41cd
Nick Arora <arora.nick@gmail.com>
2015-11-28 02:22:50 -0800

    launcher redirects to a todosActivity

--- app/src/main/java/nick/arora/todo2015/LauncherActivity.java
+++ app/src/main/java/nick/arora/todo2015/LauncherActivity.java
@@ -1,12 +1,22 @@
 package nick.arora.todo2015;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import nick.arora.todo2015.todos.TodosActivity;
 
 public class LauncherActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        startActivity(getDestinationIntent());
+        finish();
     }
+
+    private Intent getDestinationIntent() {
+        return new Intent(LauncherActivity.this, TodosActivity.class);
+    }
+
 }
---
+++ app/src/main/java/nick/arora/todo2015/todos/TodosActivity.java
@@ -0,0 +1,15 @@
+package nick.arora.todo2015.todos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nick.arora.todo2015.R;
+
+public class TodosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todos);
+    }
+}
