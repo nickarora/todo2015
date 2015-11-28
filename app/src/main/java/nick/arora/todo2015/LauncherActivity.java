8a95c20ce031ee1f9bf42b573ae6cfc7aceb0cc8
Nick Arora <arora.nick@gmail.com>
2015-11-28 01:48:55 -0800

    change mainactivity to launcher activity

--- app/src/main/java/nick/arora/todo2015/LauncherActivity.java
+++ app/src/main/java/nick/arora/todo2015/LauncherActivity.java
@@ -3,7 +3,7 @@ package nick.arora.todo2015;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
