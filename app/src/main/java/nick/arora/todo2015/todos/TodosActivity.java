5e9e8a9db3fd3785ceecd5525b6aa75d54180d50
Nick Arora <arora.nick@gmail.com>
2015-12-01 15:06:34 -0800

    remove unused imports

--- app/src/main/java/nick/arora/todo2015/todos/TodosActivity.java
+++ app/src/main/java/nick/arora/todo2015/todos/TodosActivity.java
@@ -2,12 +2,9 @@ package nick.arora.todo2015.todos;
 
 import android.os.Bundle;
 
-import javax.inject.Inject;
-
 import nick.arora.todo2015.BaseActivity;
 import nick.arora.todo2015.R;
 import nick.arora.todo2015.dagger.Injector;
-import nick.arora.todo2015.data.InMemoryTodosRepository;
 
 public class TodosActivity extends BaseActivity implements TodosContract.View {
 
