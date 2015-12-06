fac4e1fbf8b28a99ec1f91585e7292c10eabdd4c
Nick Arora <arora.nick@gmail.com>
2015-12-06 14:11:20 -0800

    add initialization code for Timber logging

--- app/src/main/java/nick/arora/todo2015/BaseApplication.java
+++ app/src/main/java/nick/arora/todo2015/BaseApplication.java
@@ -3,6 +3,7 @@ package nick.arora.todo2015;
 import android.app.Application;
 
 import nick.arora.todo2015.dagger.Injector;
+import timber.log.Timber;
 
 public class BaseApplication extends Application {
 
@@ -10,6 +11,10 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Injector.INSTANCE.initializeApplicationComponent(this);
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 
 }
--- app/src/main/java/nick/arora/todo2015/util/RxUtil.java
+++ app/src/main/java/nick/arora/todo2015/util/RxUtil.java
@@ -1,8 +1,10 @@
 package nick.arora.todo2015.util;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 public class RxUtil {
 
@@ -14,6 +16,18 @@ public class RxUtil {
     public static <T> Observable.Transformer<T, T> applyBackgroundSchedulers() {
         return observable -> observable.subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.io());
+    }
+
+    public abstract class TodoSubscriber<T> extends Subscriber<T> {
+
+        @Override
+        public void onCompleted() {}
+
+        @Override
+        public void onError(Throwable e) {
+            Timber.d("RxJava Error: %s", e.toString());
+        }
+
     }
 
 }
